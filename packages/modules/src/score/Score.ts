ed22f3a74d62bfe73314e3f02a206d02849cea92
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2022-12-07 21:16:15 +0530

    score companion module

---
+++ packages/modules/src/score/Score.ts
@@ -0,0 +1,90 @@
+/**
+ * Rating.
+ *
+ * * The 'rating' here is focused on ONDC focused rating logic
+ *
+ * @packageDocumentation
+ * @module Rating
+ * @preferred
+ */
+
+import type { IJournal, IJournalContent } from '@cord.network/types'
+import { Identifier, Crypto, DataUtils, SDKErrors } from '@cord.network/utils'
+import { SCORE_IDENTIFIER, SCORE_PREFIX } from '@cord.network/types'
+import { Identity } from '../identity/Identity.js'
+// import { BN } from 'bn.js'
+
+/**
+ *  Checks whether the input meets all the required criteria of an [[IScore]] object.
+ *  Throws on invalid input.
+ *
+ * @param input The potentially only partial [[IScore]].
+ *
+ */
+export function verifyDataStructure(input: IJournal): void {
+  if (!input.identifier) {
+    throw new SDKErrors.ERROR_RATING_ID_NOT_PROVIDED()
+  }
+  DataUtils.validateId(
+    Identifier.getIdentifierKey(input.identifier),
+    'Identifier'
+  )
+  if (!input.digest) {
+    throw new SDKErrors.ERROR_RATING_HASH_NOT_PROVIDED()
+  }
+  DataUtils.validateHash(input.digest, 'Rating hash')
+
+  // if (!input.controller) {
+  //   throw new SDKErrors.ERROR_RATING_OWNER_NOT_PROVIDED()
+  // }
+  // DataUtils.validateAddress(input.controller, 'Rating controller')
+}
+
+/**
+   * Creates a new [[Rating]] from an [[IRatingType]].
+ 
+   *
+   * @param rating The request from which the [[Rating]] should be generated.
+   * @param controller The identity of the [[Rating]] controller.
+   * @returns An instance of [[Rating]].
+   */
+export function fromJournalProperties(
+  journalProperties: IJournalContent,
+  controller: Identity
+): IJournal {
+  let journalScore = Number(journalProperties.score.toFixed(1)) * 10
+  journalProperties.score = journalScore
+  const journalHash = Crypto.hashObjectAsHexStr(journalProperties)
+  // const encodedEntity = journalProperties.entity.toU8a()
+
+  const journalId = Identifier.getIdentifier(
+    journalHash,
+    SCORE_IDENTIFIER,
+    SCORE_PREFIX
+  )
+  const journal = {
+    identifier: journalId,
+    entry: {
+      ...journalProperties,
+    },
+    digest: journalHash,
+    entitySignature: controller.signStr(journalHash),
+  }
+  verifyDataStructure(journal)
+  return journal
+}
+
+// /**
+//  *  Custom Type Guard to determine input being of type IRating using the RatingUtils errorCheck.
+//  *
+//  * @param input The potentially only partial IRating.
+//  * @returns Boolean whether input is of type IRating.
+//  */
+// export function isIRating(input: unknown): input is IRating {
+//   try {
+//     verifyDataStructure(input as IRating)
+//   } catch (error) {
+//     return false
+//   }
+//   return true
+// }
---
+++ packages/types/src/Score.ts
@@ -0,0 +1,70 @@
+/**
+ * @packageDocumentation
+ * @module IScore
+ */
+import { HexString } from '@polkadot/util/types.js'
+import type { IPublicIdentity } from './PublicIdentity.js'
+// import type { BN } from '@polkadot/util'
+
+export const SCORE_IDENTIFIER: number = 101
+export const SCORE_PREFIX: string = 'score:cord:'
+
+export enum ScoreType {
+  overall = 'overall',
+  delivery = 'delivery',
+}
+
+export interface IJournalContent {
+  entity: IPublicIdentity['address']
+  uid: string
+  tid: string
+  collector: IPublicIdentity['address']
+  requestor: IPublicIdentity['address']
+  scoreType: ScoreType
+  score: number
+}
+
+// export interface IJournalEntry {
+//   entity: IPublicIdentity['address']
+//   uid: string
+//   collector: IPublicIdentity['address']
+//   requestor: IPublicIdentity['address']
+//   scoreType: ScoreType
+//   score: number
+//   digest: HexString
+// }
+
+export interface IJournal {
+  identifier: string
+  entry: IJournalContent
+  digest: HexString
+  entitySignature: string
+}
+
+export interface IJournalDetails {
+  identifier: IJournal['identifier']
+  entry: IJournal['entry']
+  digest: IJournal['digest']
+}
+
+export interface IScoreAggregateDetails {
+  entity: IJournalContent['entity']
+  scoreType: ScoreType
+  aggregate: {
+    count: number
+    score: number
+  }
+}
+export interface IScoreAverageDetails {
+  entity: IJournalContent['entity']
+  scoreType: ScoreType
+  average: {
+    count: number
+    score: number
+  }
+}
+// export interface IJournal {
+//   identifier: string
+//   entry: IJournalEntry
+//   entitySignature: string
+// }
