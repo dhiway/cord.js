ed6bca1418383cace836148e8a558cfb2ea9e333
Amar Tumballi <amar@dhiway.com>
2023-02-21 16:12:36 +0530

    fix the jsonld expansion of object
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- packages/modules/src/content/Content.ts
+++ packages/modules/src/content/Content.ts
@@ -48,8 +48,22 @@ function jsonLDcontents(
       ...jsonabc.sortObj(contents ?? {}),
     }
   }
+
+
   Object.entries(contents || {}).forEach(([key, value]) => {
-      result[vocabulary + key] = (typeof value === 'object') ?  jsonabc.sortObj(value) : value;
+      let val = value;
+      if (typeof value === 'object') {
+          /* FIXME: GH-issue #40 */
+          /* Supporting object inside is tricky, and jsonld expansion is even more harder */
+	  /* for now, we got things under control with this check but need more work here */
+
+          let newObj = {};
+          Object.entries(jsonabc.sortObj(value)).forEach(([k,v]) => {
+	     newObj[vocabulary + k] = v;
+	  })
+	  val = newObj;
+      }
+      result[vocabulary + key] = val;
   })
   return result
 }
