e8a81e1f7d29bf9cb7fa34233872e47b7829a6c2
Amar Tumballi <amar@dhiway.com>
2023-03-13 15:11:12 +0530

    fix the rebase issues
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- packages/modules/src/content/Content.ts
+++ packages/modules/src/content/Content.ts
@@ -32,13 +32,27 @@ function jsonLDcontents(
 
   if (!expanded) {
     return {
-      ...result,
+      ...jsonabc.sortObj(result),
       '@context': { '@vocab': vocabulary },
-      ...contents,
+      ...jsonabc.sortObj(contents ?? {}),
     }
   }
+
+
   Object.entries(contents || {}).forEach(([key, value]) => {
-    result[vocabulary + key] = value
+      let val = value;
+      if (typeof value === 'object') {
+          /* FIXME: GH-issue #40 */
+          /* Supporting object inside is tricky, and jsonld expansion is even more harder */
+	  /* for now, we got things under control with this check but need more work here */
+
+          let newObj = {};
+          Object.entries(jsonabc.sortObj(value)).forEach(([k,v]) => {
+	     newObj[vocabulary + k] = v;
+	  })
+	  val = newObj;
+      }
+      result[vocabulary + key] = val;
   })
   return result
 }
