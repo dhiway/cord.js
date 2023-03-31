849e88b8d45ee0dbbdaa9052ce85249d9f51502c
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-03-31 20:54:13 +0530

    schema type and DID conformance update

--- packages/types/src/Content.ts
+++ packages/types/src/Content.ts
@@ -5,15 +5,20 @@
 import type { SchemaId } from './Schema'
 import type { DidUri } from './DidDocument'
 
+type ContentPrimitives = string | number | boolean
+
 /**
  * The minimal partial stream from which a JSON-LD representation can be built.
  */
 export type PartialContent = Partial<IContent> & Pick<IContent, 'schemaId'>
 
-export type IContents = Record<
-  string,
-  Record<string, unknown> | string | number | boolean
->
+export interface IContents {
+  [key: string]:
+    | ContentPrimitives
+    | IContents
+    | Array<ContentPrimitives | IContents>
+}
+
 export interface IContent {
   schemaId: SchemaId
   contents: IContents
