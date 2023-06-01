7950b2eb450c30c43cb16ef5e39d83f8afa8c313
Adi <adibhagawath@gmail.com>
2023-06-01 12:54:31 +0530

    Update Chain.ts (#70)

--- packages/network/src/chain/Chain.ts
+++ packages/network/src/chain/Chain.ts
@@ -176,6 +176,6 @@ export async function signAndSubmitTx(
     ...opts
   }: Partial<SubscriptionPromise.Options> & Partial<{ tip: AnyNumber }> = {}
 ): Promise<ISubmittableResult> {
-  const signedTx = await tx.signAsync(signer, { tip })
+  const signedTx = await tx.signAsync(signer, { tip, nonce:-1 })
   return submitSignedTx(signedTx, opts)
 }
