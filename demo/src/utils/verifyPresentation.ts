4839d21c8a85f1d0cfc1d1e10098e54706bb16a3
vikastc <vikas.tc@dhiway.com>
2023-06-13 16:36:30 +0530

    api.query for module 'stream'
    Signed-off-by: vikastc <vikas.tc@dhiway.com>

--- demo/src/utils/verifyPresentation.ts
+++ demo/src/utils/verifyPresentation.ts
@@ -1,4 +1,5 @@
 import * as Cord from '@cord.network/sdk'
+import { API_URL } from '../../../packages/network/src/chain/Chain'
 
 /**
  * It verifies a presentation by checking the stream on the blockchain and verifying the presentation
@@ -22,9 +23,31 @@ export async function verifyPresentation(
     await Cord.Document.verifyPresentation(presentation, { challenge })
 
     // Verify the credential by checking the stream on the blockchain.
-    const api = Cord.ConfigService.get('api')
+
+    // const api = Cord.ConfigService.get('api')
     const chainIdentifier = Cord.Stream.idToChain(presentation.identifier)
-    const streamOnChain = await api.query.stream.streams(chainIdentifier)
+    // const streamOnChain = await api.query.stream.streams(chainIdentifier)
+
+    const url = API_URL
+    const cordApiUrl = `${url}/query/stream/${chainIdentifier}`
+
+    let streamOnChain: any
+
+    if (url) {
+      streamOnChain = await fetch(cordApiUrl, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((resp) => {
+          return resp.json()
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } else {
+      console.log('URL not found')
+    }
+
     const stream = Cord.Stream.fromChain(streamOnChain, chainIdentifier)
     if (stream.revoked) {
       return false
