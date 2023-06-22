d7acfa6e9a3faef6950dd37c4e1c014033babeee
vikastc <vikas.tc@dhiway.com>
2023-06-22 13:18:27 +0530

    removed .then because of await
    Signed-off-by: vikastc <vikas.tc@dhiway.com>

--- helper.ts
+++ helper.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import fetch from 'node-fetch'
 import type { SubmittableExtrinsic } from '@cord.network/types'
 import { API_URL } from './packages/network/src/chain/Chain'
@@ -11,48 +10,41 @@ export async function cord_api_query(
   const url = API_URL
   const cordApiUrl = `${url}/query/${modules}/${section}/${identifier}`
 
-  let resp: any
-
   if (url) {
-    resp = await fetch(cordApiUrl, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((data) => {
-        return data.json()
+    try {
+      const resp = await fetch(cordApiUrl, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
       })
-      .catch((error) => {
-        console.error(error)
-      })
+      const data = resp.json()
+      return data
+    } catch (error) {
+      return error
+    }
   } else {
     console.log('URL not found')
   }
-
-  return resp
 }
 
 export async function cordApiTx(tx: SubmittableExtrinsic, modules: any) {
-  let submit: any
-
   const url = API_URL
   const cordApiUrl = `${url}/${modules}/extrinsic`
 
   if (url) {
-    submit = await fetch(cordApiUrl, {
-      body: JSON.stringify({
-        extrinsic: tx.toHex(),
-      }),
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((resp) => {
-        return resp.json()
+    try {
+      const submit = await fetch(cordApiUrl, {
+        body: JSON.stringify({
+          extrinsic: tx.toHex(),
+        }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
       })
-      .catch((error) => {
-        console.error(error)
-      })
+      const data = submit.json()
+      return data
+    } catch (error) {
+      return error
+    }
   } else {
     console.log('URL not found')
   }
-  return submit
 }
