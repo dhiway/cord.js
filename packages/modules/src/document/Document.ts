772c1df2e2e458478d7658bb63d9aeaf42d557ee
Adi <adibhagawath@gmail.com>
2023-07-11 10:13:21 +0530

    Update Document.ts

--- packages/modules/src/document/Document.ts
+++ packages/modules/src/document/Document.ts
@@ -380,7 +380,7 @@ export async function updateFromContent(
 ) {
   options = {}
   const { evidenceIds, expiresAt, templates = [], labels } = options
-  conole.log(evidenceIds, expiresAt, templates, labels)
+  console.log(evidenceIds, expiresAt, templates, labels)
 
   const newContent = Content.fromSchemaAndContent(
     schema,
