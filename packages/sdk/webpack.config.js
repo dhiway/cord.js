73bad8cb664969d725dfb9e41afd24b924dad223
Amar Tumballi <amar@dhiway.com>
2024-02-13 11:46:09 +0530

    fix: upgrade all dependant packages with yarn upgrade-interactive
    * remove loggerfactory from library for now, as it gave compatibility issues when imported in other projects
    
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- packages/sdk/webpack.config.js
+++ packages/sdk/webpack.config.js
@@ -2,8 +2,9 @@
 const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  mode: 'production',
+const nodeConfig = {
+    mode: 'production',
+    target: 'node',
   // build two different bundles from the transpiled js
   entry: {
     sdk: './lib/cjs/index.js',
@@ -29,3 +30,33 @@ module.exports = {
     minimizer: [new TerserPlugin({ include: /\.min\.umd\.js$/ })],
   },
 }
+
+const webConfig = {
+  mode: 'production',
+  // build two different bundles from the transpiled js
+  entry: {
+    sdk: './lib/cjs/index.js',
+    'sdk.min': './lib/cjs/index.js',
+  },
+  output: {
+    filename: '[name]-web.umd.js',
+    path: path.resolve(__dirname, 'dist/'),
+    libraryTarget: 'umd',
+    library: 'cord',
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.d.ts', '.mjs', '.json'],
+    symlinks: false,
+  },
+  stats: {
+    errorDetails: true,
+  },
+  optimization: {
+    minimize: true,
+    // only minimize the *.min* bundle output
+    minimizer: [new TerserPlugin({ include: /\.min\.umd\.js$/ })],
+  },
+}
+
+module.exports = [ nodeConfig, webConfig ];
