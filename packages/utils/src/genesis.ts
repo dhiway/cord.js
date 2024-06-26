ac51535318b56a209e9002ed37bc588acad378b0
Shreevatsa N <vatsa@dhiway.com>
2024-06-26 16:34:49 +0530

    get genesis with ApiPromise
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- packages/utils/src/genesis.ts
+++ packages/utils/src/genesis.ts
@@ -1,13 +1,34 @@
-import { connect, disconnect } from '@cord.network/config'
-import { SDKErrors } from '.'
+import { connect, disconnect } from '@cord.network/config';
+import { SDKErrors } from '.';
+import { ApiPromise } from '@polkadot/api';
 
 /**
- * Connects to chain and returnns the genesis hash in hexadecimal format for a given networkAddress.
+ * Returns the genesis hash in hexadecimal format for a given network object which is a ApiPromise.
+ *
+ * @param network network object of type ApiPromise for which the genesis hash is required.
+ * @returns Returns the genesis hash in Hexadecimal format.
+ */
+export async function getGenesisHash(
+  network: ApiPromise,
+): Promise<string> {
+try {
+  return network.genesisHash.toHex();
+} catch (error) {
+  const errorMessage =
+      error instanceof Error ? error.message : JSON.stringify(error)
+
+  throw new SDKErrors.CordQueryError(
+      `Error querying asset entry: ${errorMessage}`
+  )}
+}
+
+/**
+ * Connects to chain and returns the genesis hash in hexadecimal format for a given networkAddress.
  *
  * @param networkAddress Network Address for which the genesis hash is required.
  * @returns Returns the genesis hash in Hexadecimal format.
  */
-export async function getGenesisHash(
+export async function getGenesisHashWithConnect(
   networkAddress: string,
 ): Promise<string> {
 try {
