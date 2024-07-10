8d39e9262a2d266b8e379eb19cac039b8acd61c6
Shree Vatsa N <vatsa@dhiway.com>
2024-07-10 12:17:12 +0530

    utils: Add genesis functionality to multichain (#229)
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- packages/config/src/Config.ts
+++ packages/config/src/Config.ts
@@ -25,6 +25,7 @@ import {
   cordSignedExtensions,
 } from '@cord.network/type-definitions'
 import * as ConfigService from './Service.js'
+import { SDKErrors } from '@cord.network/utils'
 
 /**
  * Initializes the CORD SDK configuration and prepares cryptographic modules.
@@ -145,4 +146,50 @@ export async function disconnect(
   ConfigService.unset(network)
   await api.disconnect()
   return true
+}
+
+/**
+ * Returns the genesis hash in hexadecimal format for a given network object which is a ApiPromise.
+ *
+ * @param network network object of type ApiPromise for which the genesis hash is required.
+ * @returns Returns the genesis hash in Hexadecimal format.
+ */
+export async function getGenesisHash(
+  network: ApiPromise,
+): Promise<string> {
+try {
+  return network.genesisHash.toHex();
+} catch (error) {
+  const errorMessage =
+      error instanceof Error ? error.message : JSON.stringify(error)
+
+  throw new SDKErrors.CordQueryError(
+      `Error querying asset entry: ${errorMessage}`
+  )}
+}
+
+/**
+ * Connects to chain and returns the genesis hash in hexadecimal format for a given networkAddress.
+ *
+ * @param networkAddress Network Address for which the genesis hash is required.
+ * @returns Returns the genesis hash in Hexadecimal format.
+ */
+export async function getGenesisHashWithConnect(
+  networkAddress: string,
+): Promise<string> {
+try {
+  const connectData = await connect(networkAddress as string);
+  const genesisHash = connectData.genesisHash.toHex();
+
+  return genesisHash;
+} catch (error) {
+  const errorMessage =
+      error instanceof Error ? error.message : JSON.stringify(error)
+
+  throw new SDKErrors.CordQueryError(
+      `Error querying asset entry: ${errorMessage}`
+  )
+} finally {
+    disconnect();
+  }
 }
